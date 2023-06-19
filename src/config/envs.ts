e99e45f3506f6f475233d1b296f226cf7993c783
Maristane Figueiredo <maristanetecnologia@gmail.com>
2023-06-19 16:49:40 -0300

    fix: package.json

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,20 +3,11 @@ import dotenvExpand from 'dotenv-expand';
 
 export function loadEnv() {
   const path =
-    // process.env.NODE_ENV === 'test'
-    //   ? '.env.test'
-    //   : process.env.NODE_ENV === 'production'
-    //   ? '.env.production'
-    //   : '.env';
-
     process.env.NODE_ENV === 'test'
       ? '.env.test'
-      : process.env.NODE_ENV === 'production'
-      ? '.env.production'
       : process.env.NODE_ENV === 'development'
       ? '.env.development'
       : '.env';
-
   const currentEnvs = dotenv.config({ path });
   dotenvExpand.expand(currentEnvs);
 }
